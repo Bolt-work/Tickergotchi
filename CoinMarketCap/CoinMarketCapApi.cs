da90c5df0d9a5bf7961b20eb2ae6bed2878284eb
david Forbes <NuN>
2023-10-08 22:22:50 +0100

    first Commit

---
+++ CoinMarketCap/CoinMarketCapApi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CoinMarketCap;
+
+public class CoinMarketCapApi
+{
+    private static string _testAddress = "sandbox-api.coinmarketcap.com";
+    private static string _testApiKey = "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c";
+
+    private static string _activeAddress = "pro-api.coinmarketcap.com";
+    private static string _apiKey = "2c2da0e7-48ba-4850-824e-c5f8eb50de48";
+
+    public static string CallTestApi() => MakeAPICall(_testAddress, _testApiKey);
+
+    public static string CallApi() => MakeAPICall(_activeAddress, _apiKey);
+
+    private static string MakeAPICall(string address, string apiKey)
+    {
+        // pro-api.coinmarketcap.com
+        //var URL = new UriBuilder("https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest");
+        var URL = new UriBuilder($"https://{address}/v1/cryptocurrency/listings/latest");
+
+        var queryString = HttpUtility.ParseQueryString(string.Empty);
+        queryString["start"] = "1";
+        queryString["limit"] = "5000";
+        queryString["convert"] = "USD";
+
+        URL.Query = queryString.ToString();
+
+        var client = new WebClient();
+        client.Headers.Add("X-CMC_PRO_API_KEY", apiKey);
+        client.Headers.Add("Accepts", "application/json");
+        return client.DownloadString(URL.ToString());
+    }
+}
---
+++ CoinMarketCap/Program.cs
@@ -0,0 +1,21 @@
+﻿using CoinMarketCap.DataModels;
+using System.Text.Json;
+
+namespace CoinMarketCap
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var resultFilePath = "coinMarketResult.txt";
+            Console.WriteLine("Hello, World!");
+            //var result = CoinMarketCapApi.CallApi();
+            //File.WriteAllText("coinMarketResult.txt", result);
+
+            var resultJson = File.ReadAllText(resultFilePath);
+            //var results = JsonSerializer.Deserialize<RootModel>(resultJson);
+            var results = JsonSerializer.Deserialize<RootModel>(resultJson);
+            var t = 0;
+        }
+    }
+}
