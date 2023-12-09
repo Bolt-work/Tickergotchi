7679031f8f5dde6aec315e1c9914d8272fd2164d
david Forbes <NuN>
2023-12-09 00:39:14 +0000

    Added user passwords

---
+++ GotchiWeb/Authentication/GotchiAuthenticationStateProvider.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using System.Security.Claims;
+
+namespace GotchiWeb.Authentication
+{
+    public class GotchiAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly ProtectedSessionStorage _sessionStorage;
+        private ClaimsPrincipal _anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+
+        public GotchiAuthenticationStateProvider(ProtectedSessionStorage sessionStorage)
+        {
+            _sessionStorage = sessionStorage;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            try
+            {
+                var userSessionStorageResult = await _sessionStorage.GetAsync<UserSession>("UserSession");
+                var userSession = userSessionStorageResult.Success ? userSessionStorageResult.Value : null;
+                if (userSession == null)
+                    return await Task.FromResult(new AuthenticationState(_anonymous));
+
+                var claimPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new ClaimsIdentity(new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, userSession.UserName),
+                    new Claim(ClaimTypes.Role, userSession.Role)
+                }, "GotchiAuth")));
+                return await Task.FromResult(new AuthenticationState(claimPrincipal));
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromResult(new AuthenticationState(_anonymous));
+            }
+        }
+
+        public async Task UpdateAuthenticationState(UserSession userSession) 
+        {
+            ClaimsPrincipal claimsPrincipal;
+
+            if (userSession != null)
+            {
+                await _sessionStorage.SetAsync("UserSession", userSession);
+                claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, userSession.UserName),
+                    new Claim(ClaimTypes.Role, userSession.Role)
+                }));
+            }
+            else 
+            {
+                await _sessionStorage.DeleteAsync("UserSession");
+                claimsPrincipal = _anonymous;
+            }
+
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(claimsPrincipal)));
+        }
+    }
+}
---
+++ GotchiWeb/Authentication/UserSession.cs
@@ -0,0 +1,7 @@
+﻿namespace GotchiWeb.Authentication;
+
+public class UserSession
+{
+    public string UserName { get; set; }
+    public string Role { get; set; }
+}
